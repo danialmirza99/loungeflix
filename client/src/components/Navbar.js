0528ea64c75739094facdc4856e7e454542a6e5d
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-12-10 13:59:49 -0800

    add navBar.js in component folder in client folder

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const styles = {
+  navbarStyle: {
+    background: 'skyblue',
+    justifyContent: 'flex-end',
+    display: 'flex'
+  },
+  activePage:{
+    margin: '10px',
+    color: "goldenrod",
+    textShadow: "2px 2px 1px black, 0 0 25px maroon, 0 0 5px darkblue",
+  },
+};
+
+
+
+function Navbar({setCurrentPage, currentPage}) {
+  return (
+    <nav style={styles.navbarStyle}>
+      <a href="#"className="linkStyle"onClick={()=>{setCurrentPage("movieList")}}>Movie List</a>
+      <a href="#"className="linkStyle"onClick={()=>{setCurrentPage("home")}}>Home</a>
+      <a href="#"className="linkStyle"onClick={()=>{setCurrentPage("loginSignUp")}}>Login/Sign Up</a>
+    </nav>
+  );
+}
+
+export default Navbar;
