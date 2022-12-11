6c187084a910f609ea074e0c3b990c35e4bb5d79
A.Klein <allenkleined@gmail.com>
2022-12-11 12:09:15 -0800

    inital function for signup

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+import {
+  Form,
+  FormControl,
+  FormGroup,
+  Col,
+  Button
+  //ControlLabel
+  
+} from 'react-bootstrap';
+
+
+const Signup = () => {
+    const [formState, setFormState] = useState({
+        username: '',
+        email: '',
+        password: '',
+      });
+      const [addUser, { error, data }] = useMutation(ADD_USER);
+      const handleChange = (event) => {
+        const { name, value } = event.target;
+    
+        setFormState({
+          ...formState,
+          [name]: value,
+        });
+      };
