9a0cadd6aa418d2f03aa971c8159ff2c10511892
A.Klein <allenkleined@gmail.com>
2022-12-12 14:53:44 -0800

    changed navbar to display based on logged in or not

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,7 +6,7 @@ import Logo from '../assets/images/Lounge_Flix_Logo.jpg'
 //import SignUpForm from './SignupForm';
 //import LoginForm from './LoginForm';
 
-//import Auth from '../utils/auth';
+import Auth from '../utils/auth';
 
 const styles = {
   navbarStyle: {
@@ -42,14 +42,23 @@ const styles = {
         <Nav.Link as={Link} to='/'style={styles.linkFont}>
           Home
         </Nav.Link>
-            <Nav.Link as={Link} to='/login'style={styles.linkFont}> Login
-            </Nav.Link>
-            <Nav.Link as={Link} to='/signup'style={styles.linkFont}>Sign Up
-            </Nav.Link>
-            <Nav.Link as={Link} to='/movies'style={styles.linkFont}>Movielist
-            </Nav.Link>
-            <Nav.Link as={Link} to='/reviews'style={styles.linkFont}>Reviews
-            </Nav.Link>
+            {Auth.loggedIn() ? (
+                <>
+                  <Nav.Link as={Link} to='/movies'>
+                    MovieList
+                  </Nav.Link>
+                  <Nav.Link as={Link} to='/reviews'style={styles.linkFont}>Reviews
+                  </Nav.Link>
+                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+                </>
+              ) : (
+                <>
+                <Nav.Link as={Link} to='/login'style={styles.linkFont}> Login
+                </Nav.Link>
+                <Nav.Link as={Link} to='/signup'style={styles.linkFont}>Sign Up
+                </Nav.Link>
+                </>
+              )}
             
           </Nav>
         
